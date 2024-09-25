ac671929a6226fb75f0fb835d393ecac8605e705
ljllanosl <ljllanosl91@gmail.com>
2024-09-25 01:09:48 -0500

    fin de seccion 5

---
+++ src/app/dbz/components/form/form.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-add-character',
+  templateUrl: './form.component.html',
+})
+export class FormComponent {
+
+  @Output()
+  onNewCharacter: EventEmitter<Character> = new EventEmitter()
+
+  public character: Character = {
+    name: '',
+    power: 0
+  }
+
+  emitCharacter(): void {
+    console.log(this.character)
+    if (this.character.name.length === 0) return
+    this.onNewCharacter.emit(this.character)
+    this.character = { name: '', power: 0 }
+  }
+}
